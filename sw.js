69ffa7b0df0528632a3ad92d87cdea78453a4cf6
toanthien121314-sys <toanthien121314@gmail.com>
2026-04-23 10:19:37 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'meow-garden-v2';
+const CACHE_NAME = 'bee-girl-v3';
 const urlsToCache = [
   './',
   './index.html',
@@ -7,22 +7,18 @@ const urlsToCache = [
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
-// Cài đặt Service Worker và lưu cache
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-// Phục vụ dữ liệu từ cache nếu có
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
